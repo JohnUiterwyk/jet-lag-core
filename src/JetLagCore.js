d81ccbe645118389dda978eb270cbdabad077620
John Uiterwyk <jnuiterwyk@gmail.com>
2016-03-31 18:27:43 +1100

    renenable duration for sleep event

--- src/JetLagCore.js
+++ src/JetLagCore.js
@@ -120,7 +120,7 @@ JetLag.Core.prototype.getPlan = function(config)
     var nextWake = sleepStart.clone().add(sleepDuration);
     var mbtNext = mbtStart.clone();
     plan.minBodyTempEvents.addEvent(JetLag.Constants.EVENT_TYPE_MBT,mbtNext.clone(),moment.duration(0));
-    plan.sleepEvents.addEvent(JetLag.Constants.EVENT_TYPE_SLEEP,nextSleep.clone(),moment.duration(0));
+    plan.sleepEvents.addEvent(JetLag.Constants.EVENT_TYPE_SLEEP,nextSleep.clone(),sleepDuration);
     plan.sleepEvents.addEvent(JetLag.Constants.EVENT_TYPE_WAKE,nextWake.clone(),moment.duration(0));
     for(var i=0;i<mbtDaysToShift;i++)
     {
@@ -150,7 +150,7 @@ JetLag.Core.prototype.getPlan = function(config)
             }
         }
         nextWake = nextSleep.clone().add(sleepDuration);
-        var nextSleepEvent = plan.sleepEvents.addEvent(JetLag.Constants.EVENT_TYPE_SLEEP,nextSleep.clone(),moment.duration(0));
+        var nextSleepEvent = plan.sleepEvents.addEvent(JetLag.Constants.EVENT_TYPE_SLEEP,nextSleep.clone(),sleepDuration);
         var nextWakeEvent = plan.sleepEvents.addEvent(JetLag.Constants.EVENT_TYPE_WAKE,nextWake.clone(),moment.duration(0));
 
         var seekLight, seekDark;
